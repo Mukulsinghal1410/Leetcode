f4e88f5e60916111f3e16ac28400f70ea08b6614
Mukul Singhal <62379156+Mukulsinghal1410@users.noreply.github.com>
2022-04-18 21:19:52 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (87.53%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
@@ -13,22 +13,27 @@
  *     }
  * }
  */
-class Solution {
+class Solution 
+{
     public static int ans = 0;
-public int kthSmallest(TreeNode root, int k) {
-    helper(root, k);
-    return ans;
-}
+    public int kthSmallest(TreeNode root, int k) 
+    {
+      helper(root, k);
+      return ans;
+    }
 
-public int helper(TreeNode root, int k) {
-    if (root == null) {
+    public int helper(TreeNode root, int k) 
+    {
+      if (root == null) 
+      {
         return 0;
-    }
-    int leftCount = helper(root.left, k);
-    int rightCount = helper(root.right, k - leftCount - 1);
-    if (k == leftCount + 1) {
+      }
+      int leftCount = helper(root.left, k);
+      int rightCount = helper(root.right, k - leftCount - 1);
+      if (k == leftCount + 1) 
+      {
         ans = root.val;
+      }
+      return leftCount + rightCount + 1;
     }
-    return leftCount + rightCount + 1;
-}
 }
