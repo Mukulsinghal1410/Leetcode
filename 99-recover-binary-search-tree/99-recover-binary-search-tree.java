27491f43ed1738144ca494a2a704aa9e05dd99fe
Mukul Singhal <62379156+Mukulsinghal1410@users.noreply.github.com>
2022-04-19 21:10:12 +0530

    Time: 3 ms (69.35%), Space: 47.2 MB (54.40%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.java
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.java
@@ -14,40 +14,25 @@
  * }
  */
 class Solution {
-    TreeNode prev = null;TreeNode first = null; TreeNode second = null;
-    
-    public void inorderHelper(TreeNode root)
-    {
-        if(root == null)  return;
-        
-        inorderHelper(root.left);
-        
-        if(first == null && prev != null && prev.val >= root.val)
-            first = prev;
-        
-        if(first != null && prev.val >= root.val)
-            second = root;
-        
-        prev = root;
-        
-        inorderHelper(root.right);
-    }
-    
-    public void swap(TreeNode first, TreeNode second)
-    {
+    TreeNode prev = null, first = null, second = null;
+
+    public void recoverTree(TreeNode root) {
+        evalSwappedNodes(root);
         int temp = first.val;
-        
         first.val = second.val;
         second.val = temp;
     }
-    
-    public void recoverTree(TreeNode root)
-    {
-        prev = null; first = null; second = null;
-        
-        inorderHelper(root);
-        
-        swap(first,second);
-    
+
+    private void evalSwappedNodes(TreeNode curr) {
+        if (curr == null)
+            return;
+        evalSwappedNodes(curr.left);
+        if (prev != null && prev.val > curr.val) {
+            if (first == null)
+                first = prev;
+            second = curr;
+        }
+        prev = curr;
+        evalSwappedNodes(curr.right);
     }
 }
