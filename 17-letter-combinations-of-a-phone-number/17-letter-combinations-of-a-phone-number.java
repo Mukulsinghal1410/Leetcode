f39d726a696b16c206b0520968916a4750f298f8
Mukul Singhal <62379156+Mukulsinghal1410@users.noreply.github.com>
2022-05-09 17:48:28 +0530

    Time: 6 ms (35.51%), Space: 43.6 MB (6.53%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -1,25 +1,30 @@
-class Solution 
-{
-	List<String> list = new ArrayList<>();
-	List<String> k = new ArrayList<>(Arrays.asList("","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"));
-
-	public void fun(String s,int start,String str)
-	{
-		if(start==s.length())
-		{
-			list.add(str);
-			return;
-		}
-		String x = k.get(s.charAt(start)-'0');
-		for(int i=0;i<x.length();i++)
-			fun(s,start+1,str+x.charAt(i));
-	}
-
-	public List<String> letterCombinations(String digits) 
-	{
-		if(digits.length()==0)
-			return list;
-		fun(digits,0,"");
-		return list;
-	}
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        List<String> s = new ArrayList<>();
+        
+        if(digits == null || digits.length() == 0)
+            return s;
+        
+        int len = digits.length();
+        int arr[] = new int[len];
+        for(int i =0 ;i< len ; i++){
+            arr[i]  = digits.charAt(i)-'0';
+        }
+        
+        String letters[] = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        Queue<String> que = new LinkedList<>();
+        que.offer(""); //better then queue.add() as it does not throw run time exception
+        
+        for(int i =0 ; i< len; i++){
+            String word = letters[arr[i]-2];
+            int size = que.size();
+            for (int j = 0; j < size; j++) {
+                String temp = que.poll();
+                for (char ch : word.toCharArray()) {
+                    que.offer(temp + ch);
+                }
+            }
+        }
+        return new ArrayList<>(que);
+    }
 }
