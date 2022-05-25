cf05bbb8e8d8efed4ea2f43015a6d8c294d47ddb
Mukul Singhal <62379156+Mukulsinghal1410@users.noreply.github.com>
2022-05-25 20:11:43 +0530

    Time: 110 ms (12.09%), Space: 77.5 MB (86.69%) - LeetHub

--- 354-russian-doll-envelopes/354-russian-doll-envelopes.java
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.java
@@ -1,43 +1,28 @@
-class Solution 
-{
-    public int maxEnvelopes(int[][] envelopes) 
-    {
-        int n = envelopes.length, left, right, mid;
-        // Sort array
-        Arrays.sort(envelopes, new Comparator<int[]>() 
-                    {
-            public int compare(int[] a, int[] b) 
-            {
-                if (a[0] == b[0]) 
-                {
-                    return Integer.compare(b[1], a[1]);
-                } else {
-                    return Integer.compare(a[0], b[0]);
-                }
-            }
-        });
+class Solution {
+    public int maxEnvelopes(int[][] arr) {
+         int n = arr.length;
+        Arrays.sort(arr,(p,q)-> p[0]==q[0] ? q[1]-p[1] : p[0]-q[0]);
         
-        List<Integer> list = new ArrayList<>();
-        list.add(envelopes[0][1]);
+         int dp[] = new int[arr.length];
+    int len = 0;
+    for(int[] envelope : arr){
+        int index = binarySearch(dp, 0, len, envelope[1]);
         
-        for (int i=1; i<n; i++) {
-            if (envelopes[i][1] > list.get(list.size() - 1)) {
-                list.add(envelopes[i][1]);
-            } else {
-                // Find pos of first element which is >= current element
-                left = 0; 
-                right = list.size() - 1;
-                while (left < right) {
-                    mid = left + (right - left) / 2;
-                    if (list.get(mid) >= envelopes[i][1]) {
-                        right = mid;
-                    } else {
-                        left = mid + 1;
-                    }
-                }
-                list.set(right, envelopes[i][1]);
-            }
-        }
-        return list.size();
+        dp[index] = envelope[1];
+        if(index == len)
+            len++;
+    }
+    return len;
+        
+    }
+    
+    public int binarySearch(int[] dp,int si, int ei, int target) {
+         while(si<ei){
+             int mid = si+(ei-si)/2;
+             if(dp[mid] == target) return mid;
+             else if(dp[mid] > target) ei = mid;
+             else  si = mid+1;
+         }
+        return si;
     }
 }
