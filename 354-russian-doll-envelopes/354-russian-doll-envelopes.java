031c43b3b00bcd0dc8067e1938363bc8845ca7fd
Mukul Singhal <62379156+Mukulsinghal1410@users.noreply.github.com>
2022-05-25 20:12:55 +0530

    Time: 59 ms (79.47%), Space: 86.7 MB (19.10%) - LeetHub

--- 354-russian-doll-envelopes/354-russian-doll-envelopes.java
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.java
@@ -1,28 +1,30 @@
-class Solution {
-    public int maxEnvelopes(int[][] arr) {
-         int n = arr.length;
-        Arrays.sort(arr,(p,q)-> p[0]==q[0] ? q[1]-p[1] : p[0]-q[0]);
+public class Solution {
+    public int maxEnvelopes(int[][] envelopes) {
+        if(envelopes.length < 2) return envelopes.length;
         
-         int dp[] = new int[arr.length];
-    int len = 0;
-    for(int[] envelope : arr){
-        int index = binarySearch(dp, 0, len, envelope[1]);
+        Arrays.sort(envelopes, new EnvelopeComparator());
+        int[] dp = new int[envelopes.length];
+        int size = 0;
         
-        dp[index] = envelope[1];
-        if(index == len)
-            len++;
-    }
-    return len;
-        
+        for(int[] envelope: envelopes) {
+            // binary search
+            int left = 0, right = size, middle = 0;     // right = size
+            while(left < right) {
+                middle = left + (right - left) / 2;
+                if(dp[middle] < envelope[1]) left = middle + 1;
+                else right = middle;
+            }
+            
+            // left is the right position to 'replace' in dp array
+            dp[left] = envelope[1];
+            if(left == size) size++;
+        }
+        return size;
     }
     
-    public int binarySearch(int[] dp,int si, int ei, int target) {
-         while(si<ei){
-             int mid = si+(ei-si)/2;
-             if(dp[mid] == target) return mid;
-             else if(dp[mid] > target) ei = mid;
-             else  si = mid+1;
-         }
-        return si;
+    class EnvelopeComparator implements Comparator<int[]> {
+        public int compare(int[] e1, int[] e2) {
+            return e1[0] == e2[0] ? e2[1] - e1[1] : e1[0] - e2[0];
+        }
     }
 }
