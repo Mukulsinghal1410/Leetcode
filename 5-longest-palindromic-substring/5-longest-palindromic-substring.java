e2d1afdc9a88805621eec24b1b32a3232cf30657
Mukul Singhal <62379156+Mukulsinghal1410@users.noreply.github.com>
2022-06-16 17:18:58 +0530

    Time: 408 ms (24.54%), Space: 93.6 MB (16.55%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,33 +1,37 @@
-public class Solution 
+class Solution 
 {
-  private int lo, maxLen;
-
-  public String longestPalindrome(String s) 
-  {
-	  int len = s.length();
-	  if (len < 2)
-		  return s;
-	
-    for (int i = 0; i < len-1; i++) 
-    {
-     	extendPalindrome(s, i, i);  //assume odd length, try to extend Palindrome as possible
-      
-     	extendPalindrome(s, i, i+1); //assume even length.
+    
+    boolean[][] memo ;
+    
+    public String longestPalindrome(String s) {
+        
+        int len = s.length();
+        
+        int maxLen = 0;
+        String res = "";
+        memo = new boolean[len][len];
+        
+        for (int left = len -1; left >= 0; left--){ // notice this goes backwards
+            for (int right = left; right < len; right++){
+                
+             if (s.charAt(left) == s.charAt(right)){
+                 
+                if (right - left <=2){
+                    memo[left][right] = true;
+                } else{
+                    memo[left][right] = memo[left + 1][right - 1]; 
+                }
+             }
+    
+            if (memo[left][right]  && maxLen < right - left + 1) {
+                maxLen = right - left + 1;
+                res = s.substring(left, right + 1);
+                }
+            }   
+            
+        }
+        
+        return res;
     }
-    return s.substring(lo, lo + maxLen);
-}
-
-  private void extendPalindrome(String s, int j, int k) 
-  {
-	  while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) 
-    {
-		  j--;
-		  k++;
-	  }
-	  if (maxLen < k - j - 1) 
-    {
-		  lo = j + 1;
-		  maxLen = k - j - 1;
-	  }
-  }
+    
 }
