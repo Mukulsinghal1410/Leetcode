cf657f86e7db907e45b265a3e7f433ac5f60ad04
Mukul Singhal <62379156+Mukulsinghal1410@users.noreply.github.com>
2022-06-29 11:40:07 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (11.96%) - LeetHub

--- 66-plus-one/66-plus-one.java
+++ 66-plus-one/66-plus-one.java
@@ -2,16 +2,16 @@ class Solution
 {
     public int[] plusOne(int[] digits) 
     {
-        
-        for(int i = digits.length - 1; i >= 0; i--)
-        {
-            if(++digits[i] != 10) 
-              return digits;
-            digits[i] = 0;
-        }
-        int [] res = new int[digits.length + 1];
-        
-        res[0] = 1;
-        return res;
+      int carry = 1;
+      for (int i = digits.length-1; i>= 0; i--) 
+      {
+        digits[i] += carry;
+        if (digits[i] <= 9) // early return 
+            return digits;
+        digits[i] = 0;
     }
+    int[] ret = new int[digits.length+1];
+    ret[0] = 1;
+    return ret;
+}
 }
