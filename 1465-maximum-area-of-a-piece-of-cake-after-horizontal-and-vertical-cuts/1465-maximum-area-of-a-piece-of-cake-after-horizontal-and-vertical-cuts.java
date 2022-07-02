226c9acc7f895bb8e69eb9daa62fa831eb5b5736
Mukul Singhal <62379156+Mukulsinghal1410@users.noreply.github.com>
2022-07-02 13:03:31 +0530

    Time: 18 ms (82.54%), Space: 61.2 MB (33.41%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.java
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.java
@@ -1,18 +1,27 @@
-class Solution 
-{
-    public int maxArea(int h, int w, int[] hc, int[] vc) 
-    {
-        Arrays.sort(hc);
-        Arrays.sort(vc);
-        int maxh = Math.max(hc[0], h - hc[hc.length-1]),
-            maxv = Math.max(vc[0], w - vc[vc.length-1]);
-      
-        for (int i = 1; i < hc.length; i++)
-            maxh = Math.max(maxh, hc[i] - hc[i-1]);
-      
-        for (int i = 1; i < vc.length; i++)
-            maxv = Math.max(maxv, vc[i] - vc[i-1]);
-      
-        return (int)((long)maxh * maxv % 1000000007);
-    }
+class Solution {
+	public static int maxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts) {
+		int mod = (int) 1e9 + 7;
+
+		// Soring the arrays to get the cuts in increasing order
+		Arrays.sort(horizontalCuts);
+		Arrays.sort(verticalCuts);
+
+		// Calculating the maximum distance between two adjacent cuts and the edges of the cake
+		int hN = horizontalCuts.length, wN = verticalCuts.length;
+		long maxH = horizontalCuts[0], maxW = verticalCuts[0];
+
+		maxH = Math.max(maxH, h - horizontalCuts[hN - 1]);
+		maxW = Math.max(maxW, w - verticalCuts[wN - 1]);
+
+		// Looping through the array to get the maximum difference between two adjacent cuts
+		for (int i = 1; i < hN; i++) {
+			maxH = Math.max(maxH, (horizontalCuts[i] - horizontalCuts[i - 1]));
+		}
+		for (int i = 1; i < wN; ++i) {
+			maxW = Math.max(maxW, (verticalCuts[i] - verticalCuts[i - 1]));
+		}
+
+		// Returning the "maximum area of a piece of cake"
+		return (int) ((maxH % mod * maxW % mod) % mod);
+	}
 }
