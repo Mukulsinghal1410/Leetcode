c8f127a6f5c87ba9c9484454504869a5c17a7fff
Mukul Singhal <62379156+Mukulsinghal1410@users.noreply.github.com>
2022-07-20 12:44:18 +0530

    Time: 2 ms (57.62%), Space: 42 MB (26.74%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.java
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.java
@@ -3,7 +3,7 @@ class Solution {
     {
       List<Integer> row = new ArrayList<>();
       
-      for(int i=0; i<=rowIndex; i++)
+      for(int i=0; i<rowIndex+1; i++)
       {
         row.add(1);
         for(int j=i-1; j>0; j--)
